5d98591ad9e78aa3045ac14411a28ca225b56993
KikoPT1234 <carricokiko2006@gmail.com>
2020-12-15 22:10:40 +0100

    env var test 3

--- pages/_app.js
+++ pages/_app.js
@@ -701,12 +701,12 @@ export default function MinehutXYZ(props) {
 		// Your web app's Firebase configuration
 		// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 		var firebaseConfig = {
-			apiKey: "AIzaSyDbUYPILRhBLUVuIQfLaohSfAmc6miWY-8",
+			apiKey: process.env.NEXT_PUBLIC_API_KEY,
 			authDomain: "minehut-xyz.firebaseapp.com",
 			projectId: "minehut-xyz",
 			storageBucket: "minehut-xyz.appspot.com",
 			messagingSenderId: "263953233640",
-			appId: "1:263953233640:web:afc99115fb14a56179fb5d",
+			appId: process.env.NEXT_PUBLIC_API_KEY,
 			measurementId: "G-1QLTGB09K6",
 		};
 
@@ -715,8 +715,6 @@ export default function MinehutXYZ(props) {
 		firebase.analytics();
 		setInitialized(true);
 	});
-
-	console.log(process?.env?.NEXT_PUBLIC_API_KEY);
 
 	React.useEffect(() => {
 		const jssStyles = document.querySelector("#jss-server-side");
