d9e5309b29b5e02dd6bb1f14537b4746f82a9ecf
KikoPT1234 <carricokiko2006@gmail.com>
2020-12-15 21:25:08 +0100

    testing

--- pages/_app.js
+++ pages/_app.js
@@ -695,6 +695,25 @@ export default function MinehutXYZ(props) {
 	};
 
 	React.useEffect(() => {
+		// Your web app's Firebase configuration
+		// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+		var firebaseConfig = {
+			apiKey: "AIzaSyDbUYPILRhBLUVuIQfLaohSfAmc6miWY-8",
+			authDomain: "minehut-xyz.firebaseapp.com",
+			projectId: "minehut-xyz",
+			storageBucket: "minehut-xyz.appspot.com",
+			messagingSenderId: "263953233640",
+			appId: "1:263953233640:web:afc99115fb14a56179fb5d",
+			measurementId: "G-1QLTGB09K6",
+		};
+		// Initialize Firebase
+		firebase.initializeApp(firebaseConfig);
+		firebase.analytics();
+	});
+
+	console.log(process?.env?.TEST_VAR);
+
+	React.useEffect(() => {
 		const jssStyles = document.querySelector("#jss-server-side");
 		if (jssStyles) {
 			jssStyles.parentElement.removeChild(jssStyles);
