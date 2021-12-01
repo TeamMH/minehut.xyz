b337dca8cc9825381e3100545633b42b18875622
KikoPT1234 <39025735+KikoPT1234@users.noreply.github.com>
2021-12-01 17:45:50 +0100

    fix stuff

--- buildRoutes.js
+++ buildRoutes.js
@@ -6,88 +6,88 @@ const fs = require("fs");
 const path = require("path");
 
 function readFile(directory, file) {
-	if (
-		!path.extname(file).match(/mdx?$/) ||
-		path.basename(file).match(/404\.mdx?$/)
-	)
-		return;
-	const routeParents = path
-		.join(directory, file)
-		.split(path.sep)
-		.slice(1)
-		.reverse()
-		.slice(1)
-		.reverse()
-		.map((p) => {
-			let name = p
-				.replace(/-./g, (e) => " " + e[1].toUpperCase())
-				.split("")
-				.map((c, i) => (i === 0 ? c.toUpperCase() : c))
-				.join("");
-			routeOverrides.forEach((exception) => {
-				name = name.replace(
-					new RegExp(exception[0], "g"),
-					exception[1]
-				);
-			});
-			return name;
-		});
-	let name = path.basename(file).match(/index\..+$/)
-		? "Home"
-		: path
-				.basename(file)
-				.replace(/\.mdx?$/, "")
-				.replace(/-./g, (e) => " " + e[1].toUpperCase())
-				.split("")
-				.map((c, i) => (i === 0 ? c.toUpperCase() : c))
-				.join("");
+    if (
+        !path.extname(file).match(/mdx?$/) ||
+        path.basename(file).match(/404\.mdx?$/)
+    )
+        return;
+    const routeParents = path
+        .join(directory, file)
+        .split(path.sep)
+        .slice(1)
+        .reverse()
+        .slice(1)
+        .reverse()
+        .map((p) => {
+            let name = p
+                .replace(/-./g, (e) => " " + e[1].toUpperCase())
+                .split("")
+                .map((c, i) => (i === 0 ? c.toUpperCase() : c))
+                .join("");
+            routeOverrides.forEach((exception) => {
+                name = name.replace(
+                    new RegExp(exception[0], "g"),
+                    exception[1]
+                );
+            });
+            return name;
+        });
+    let name = path.basename(file).match(/index\..+$/)
+        ? "Home"
+        : path
+              .basename(file)
+              .replace(/\.mdx?$/, "")
+              .replace(/-./g, (e) => " " + e[1].toUpperCase())
+              .split("")
+              .map((c, i) => (i === 0 ? c.toUpperCase() : c))
+              .join("");
 
-	routeOverrides.forEach(
-		(exception) =>
-			(name = name.replace(new RegExp(exception[0], "g"), exception[1]))
-	);
+    routeOverrides.forEach(
+        (exception) =>
+            (name = name.replace(new RegExp(exception[0], "g"), exception[1]))
+    );
 
-	const route =
-		"/" +
-		(name === "Home"
-			? ""
-			: path
-					.join(directory, file)
-					.replace(/\\+/g, "/")
-					.split("/")
-					.slice(1)
-					.join("/")
-					.replace(/\.mdx?$/, "")
-		).toLowerCase();
+    const route =
+        "/" +
+        (name === "Home"
+            ? ""
+            : path
+                  .join(directory, file)
+                  .replace(/\\+/g, "/")
+                  .split("/")
+                  .slice(1)
+                  .join("/")
+                  .replace(/\.mdx?$/, "")
+        ).toLowerCase();
 
-	let dir = routes;
+    let dir = routes;
 
-	routeParents.forEach((parent) => {
-		if (!dir[parent]) {
-			if (dir !== routes)
-				dir[parent] = {
-					__dirroute__: route
-						.split("/")
-						.slice(0, route.split("/").length - 1)
-						.join("/"),
-				};
-			else dir[parent] = {};
-		}
-		dir = dir[parent];
-	});
+    routeParents.forEach((parent) => {
+        if (!dir[parent]) {
+            if (dir !== routes)
+                dir[parent] = {
+                    __dirroute__: route
+                        .split("/")
+                        .slice(0, route.split("/").length - 1)
+                        .join("/"),
+                };
+            else dir[parent] = {};
+        }
+        dir = dir[parent];
+    });
 
-	dir[name] = route;
+    dir[name] = route;
 }
 
 function readDir(directory) {
-	fs.readdirSync(directory, "utf-8").forEach((file) => {
-		if (file === "index.md") return;
-		if (fs.lstatSync(path.join(directory, file)).isFile()) {
-			readFile(directory, file);
-		} else {
-			readDir(path.join(directory, file));
-		}
-	});
+    fs.readdirSync(directory, "utf-8").forEach((file) => {
+        if (file === "index.md") return;
+        if (fs.lstatSync(path.join(directory, file)).isFile()) {
+            readFile(directory, file);
+        } else {
+            readDir(path.join(directory, file));
+        }
+    });
 }
 
 readFile("./pages", "index.md");
@@ -103,32 +103,31 @@ readFile("./pages/skript", "basics.md");
 readDir("./pages");
 
 routes["Plugins"]["Plugin List"] = "/plugins/plugin-list";
-routes["Server Search"] = "/server-search";
 
 function sortRoutes(routes) {
-	const sortedKeys = Object.keys(routes).sort((a, b) => {
-		if (typeof routes[a] === "object" && typeof routes[b] !== "object")
-			return 1;
-		else if (
-			(typeof routes[a] === "object" && typeof routes[b] === "object") ||
-			(typeof routes[a] === "string" && typeof routes[b] === "string")
-		)
-			return 0;
-		else return -1;
-	});
+    const sortedKeys = Object.keys(routes).sort((a, b) => {
+        if (typeof routes[a] === "object" && typeof routes[b] !== "object")
+            return 1;
+        else if (
+            (typeof routes[a] === "object" && typeof routes[b] === "object") ||
+            (typeof routes[a] === "string" && typeof routes[b] === "string")
+        )
+            return 0;
+        else return -1;
+    });
 
-	const newRoutes = {};
+    const newRoutes = {};
 
-	sortedKeys.forEach((key) => {
-		if (typeof routes[key] === "object")
-			newRoutes[key] = sortRoutes(routes[key]);
-		else newRoutes[key] = routes[key];
-	});
+    sortedKeys.forEach((key) => {
+        if (typeof routes[key] === "object")
+            newRoutes[key] = sortRoutes(routes[key]);
+        else newRoutes[key] = routes[key];
+    });
 
-	return newRoutes;
+    return newRoutes;
 }
 
 fs.writeFileSync(
-	"./routes.json",
-	JSON.stringify(sortRoutes(routes), null, "    ")
+    "./routes.json",
+    JSON.stringify(sortRoutes(routes), null, "    ")
 );
--- pages/plugins/plugin-list.js
+++ pages/plugins/plugin-list.js
@@ -1,20 +1,20 @@
 import React from "react";
 import {
-	Accordion,
-	AccordionActions,
-	AccordionDetails,
-	AccordionSummary,
-	Button,
-	Divider,
-	Grid,
-	Hidden,
-	Paper,
-	TablePagination,
-	TextField,
-	Typography,
-	makeStyles,
-	IconButton,
-	Tooltip,
+    Accordion,
+    AccordionActions,
+    AccordionDetails,
+    AccordionSummary,
+    Button,
+    Divider,
+    Grid,
+    Hidden,
+    Paper,
+    TablePagination,
+    TextField,
+    Typography,
+    makeStyles,
+    IconButton,
+    Tooltip,
 } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Head from "next/head";
@@ -23,219 +23,223 @@ import { useRouter } from "next/router";
 import { copyToClipboard } from "../../lib/utils";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		width: "100%",
-		fontSize: "1rem",
-	},
-	accordion: {
-		overflowWrap: "break-word !important",
-		wordBreak: "break-word !important",
-		whiteSpace: "pre-wrap !important",
-		fontSize: "0.5rem !important",
-		maxWidth: "100%",
-	},
-	accordionDetails: {
-		fontSize: "0.5rem !important",
-		display: "block",
-	},
-	input: {
-		width: "100%",
-	},
-	paper: {
-		margin: theme.spacing(2, 0),
-		padding: theme.spacing(2),
-	},
-	heading: {
-		margin: theme.spacing(3, 0),
-		whiteSpace: "pre-wrap !important",
-		overflowWrap: "anywhere",
-		wordBreak: "break-word",
-		position: "relative",
-		"& button": {
-			display: "none",
-		},
-		"&:hover button": {
-			display: "inline-block",
-		},
-	},
-	linkCopyButton: {
-		display: "inline-block",
-		height: "100%",
-		width: "auto",
-		position: "relative",
-		bottom: 4,
-		left: 8,
-	},
+    root: {
+        width: "100%",
+        fontSize: "1rem",
+    },
+    accordion: {
+        overflowWrap: "break-word !important",
+        wordBreak: "break-word !important",
+        whiteSpace: "pre-wrap !important",
+        fontSize: "0.5rem !important",
+        maxWidth: "100%",
+    },
+    accordionDetails: {
+        fontSize: "0.5rem !important",
+        display: "block",
+    },
+    input: {
+        width: "100%",
+    },
+    paper: {
+        margin: theme.spacing(2, 0),
+        padding: theme.spacing(2),
+    },
+    heading: {
+        margin: theme.spacing(3, 0),
+        whiteSpace: "pre-wrap !important",
+        overflowWrap: "anywhere",
+        wordBreak: "break-word",
+        position: "relative",
+        "& button": {
+            display: "none",
+        },
+        "&:hover button": {
+            display: "inline-block",
+        },
+    },
+    linkCopyButton: {
+        display: "inline-block",
+        height: "100%",
+        width: "auto",
+        position: "relative",
+        bottom: 4,
+        left: 8,
+    },
 }));
 
 export default function PluginList(props) {
-	const classes = useStyles();
+    const classes = useStyles();
 
-	const router = useRouter();
+    const router = useRouter();
 
-	const [open, setOpen] = React.useState(-1);
-	const [page, setPage] = React.useState(0);
-	const [rowsPerPage, setRowsPerPage] = React.useState(25);
-	const [value, setValue] = React.useState("");
+    const [open, setOpen] = React.useState(-1);
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(25);
+    const [value, setValue] = React.useState("");
 
-	const handleChangeRowsPerPage = (event) => {
-		setRowsPerPage(parseInt(event.target.value, 10));
-		setPage(0);
-	};
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
 
-	const [plugins, setPlugins] = React.useState(props.plugins);
+    const [plugins, setPlugins] = React.useState(props.plugins);
 
-	const handleChange = (value) => {
-		setPage(0);
-		setValue(value || "");
-		if (!value) setPlugins(props.plugins);
-		else
-			setPlugins(
-				props.plugins.filter((p) =>
-					p.title.toLowerCase().includes(value.toLowerCase())
-				)
-			);
-	};
+    const handleChange = (value) => {
+        setPage(0);
+        setValue(value || "");
+        if (!value) setPlugins(props.plugins);
+        else
+            setPlugins(
+                props.plugins.filter((p) =>
+                    p.title.toLowerCase().includes(value.toLowerCase())
+                )
+            );
+    };
 
-	React.useEffect(() => {
-		setValue(router.query.q || "");
-		handleChange(router.query.q);
-		document.querySelector("input").focus();
-	}, [router]);
+    React.useEffect(() => {
+        setValue(router.query.q || "");
+        handleChange(router.query.q);
+        document.querySelector("input").focus();
+    }, [router]);
 
-	return (
-		<div className={classes.root}>
-			<Typography variant="h4" component="h1" className={classes.heading}>
-				PLUGIN LIST
-			</Typography>
-			<Typography color="textSecondary" paragraph>
-				Look up all available plugins on Minehut!
-			</Typography>
-			<Divider />
-			<Paper className={classes.paper}>
-				<TextField
-					value={value}
-					className={classes.input}
-					label="Search for plugins"
-					onChange={(e) => handleChange(e.target.value)}
-				/>
-				<TablePagination
-					component="div"
-					count={plugins.length}
-					page={page}
-					onChangePage={(e, v) => setPage(v)}
-					rowsPerPage={rowsPerPage}
-					onChangeRowsPerPage={handleChangeRowsPerPage}
-					className={classes.pagination}
-				/>
-			</Paper>
-			<div>
-				{plugins.map((plugin, i) => {
-					if (Math.floor(i / rowsPerPage) !== page) return;
+    return (
+        <div className={classes.root}>
+            <Typography variant="h4" component="h1" className={classes.heading}>
+                PLUGIN LIST
+            </Typography>
+            <Typography color="textSecondary" paragraph>
+                Look up all available plugins on Minehut!
+            </Typography>
+            <Divider />
+            <Paper className={classes.paper}>
+                <TextField
+                    value={value}
+                    className={classes.input}
+                    label="Search for plugins"
+                    onChange={(e) => handleChange(e.target.value)}
+                />
+                <TablePagination
+                    component="div"
+                    count={plugins.length}
+                    page={page}
+                    onChangePage={(e, v) => setPage(v)}
+                    rowsPerPage={rowsPerPage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                    className={classes.pagination}
+                />
+            </Paper>
+            <div>
+                {plugins.map((plugin, i) => {
+                    if (Math.floor(i / rowsPerPage) !== page) return;
 
-					const regex = /Link:\n*(https?:\/\/.+)$/g;
-					const regex2 = /\n*(https?:\/\/.+$)/g;
-					let link = regex.exec(plugin.description);
-					if (!link || link.length === 0)
-						link = regex2.exec(plugin.description);
+                    const regex =
+                        /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g;
+                    let link = regex.exec(plugin.description);
 
-					return (
-						<Accordion
-							TransitionProps={{ unmountOnExit: true }}
-							key={plugin.sku}
-							className={classes.accordion}
-							onChange={(e, isExpanded) =>
-								setOpen(isExpanded ? i : -1)
-							}
-							expanded={open === i}
-						>
-							<AccordionSummary
-								key={plugin.sku}
-								expandIcon={<ExpandMoreIcon />}
-							>
-								<Grid
-									container
-									spacing={3}
-									justify="space-between"
-									alignItems="center"
-								>
-									<Grid xs={12} item>
-										<Typography variant="h6">
-											{plugin.title}
-										</Typography>
-									</Grid>
-								</Grid>
-							</AccordionSummary>
-							<AccordionDetails
-								key={plugin.sku}
-								className={classes.accordionDetails}
-							>
-								{plugin.desc.split("\n").map((d) => (
-									<Typography paragraph key={d}>
-										<strong>Description | </strong>
-										{d}
-									</Typography>
-								))}
-								<Typography paragraph>
-									<strong>Created At | </strong>
-									{new Date(plugin.createdAt).toUTCString()}
-								</Typography>
-								<Typography paragraph>
-									<strong>Updated at | </strong>{" "}
-									{new Date(
-										plugin.updatedAt
-									).toUTCString()}
-								</Typography>
-							</AccordionDetails>
-							<Divider />
-							<AccordionActions>
-								<Button
-									disabled={!link || link.length === 0}
-									href={
-										link && link.length > 0 ? link[1] : null
-									}
-									color="primary"
-									target="_blank"
-								>
-									Visit Plugin Website
-								</Button>
-							</AccordionActions>
-						</Accordion>
-					);
-				})}
-			</div>
-			<TablePagination
-				component={Paper}
-				count={plugins.length}
-				page={page}
-				onChangePage={(e, v) => setPage(v)}
-				rowsPerPage={rowsPerPage}
-				onChangeRowsPerPage={handleChangeRowsPerPage}
-				className={classes.paper}
-			/>
-		</div>
-	);
+                    return (
+                        <Accordion
+                            TransitionProps={{ unmountOnExit: true }}
+                            key={plugin.sku}
+                            className={classes.accordion}
+                            onChange={(e, isExpanded) =>
+                                setOpen(isExpanded ? i : -1)
+                            }
+                            expanded={open === i}
+                        >
+                            <AccordionSummary
+                                key={plugin.sku}
+                                expandIcon={<ExpandMoreIcon />}
+                            >
+                                <Grid
+                                    container
+                                    spacing={3}
+                                    justify="space-between"
+                                    alignItems="center"
+                                >
+                                    <Grid xs={12} item>
+                                        <Typography variant="h6">
+                                            {plugin.title}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </AccordionSummary>
+                            <AccordionDetails
+                                key={plugin.sku}
+                                className={classes.accordionDetails}
+                            >
+                                <Typography paragraph>
+                                    <strong>Description:</strong>
+                                </Typography>
+                                {plugin.description.split("\n").map((d) => (
+                                    <Typography paragraph key={d}>
+                                        <span
+                                            dangerouslySetInnerHTML={{
+                                                __html: d,
+                                            }}
+                                        ></span>
+                                    </Typography>
+                                ))}
+                                <Typography paragraph>
+                                    <strong>Created At | </strong>
+                                    {new Date(plugin.createdAt).toUTCString()}
+                                </Typography>
+                                <Typography paragraph>
+                                    <strong>Updated at | </strong>{" "}
+                                    {new Date(plugin.updatedAt).toUTCString()}
+                                </Typography>
+                            </AccordionDetails>
+                            <Divider />
+                            <AccordionActions>
+                                <Button
+                                    disabled={!link || link.length === 0}
+                                    href={
+                                        link && link.length > 0 ? link[0] : null
+                                    }
+                                    color="primary"
+                                    target="_blank"
+                                >
+                                    Visit Plugin Website
+                                </Button>
+                            </AccordionActions>
+                        </Accordion>
+                    );
+                })}
+            </div>
+            <TablePagination
+                component={Paper}
+                count={plugins.length}
+                page={page}
+                onChangePage={(e, v) => setPage(v)}
+                rowsPerPage={rowsPerPage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                className={classes.paper}
+            />
+        </div>
+    );
 }
 
 export async function getStaticProps() {
-	const res = await fetch("https://facade-service-prod.superleague.com/facade/v1/client/products").then(res => res.json())
-	
-	const plugins = res.filter(res => res.category === "Plugin")
+    const res = await fetch(
+        "https://facade-service-prod.superleague.com/facade/v1/client/products"
+    ).then((res) => res.json());
 
-	return {
-		props: {
-			plugins: plugins.sort((a, b) => {
-				var nameA = a.title.toUpperCase();
-				var nameB = b.title.toUpperCase();
-				if (nameA < nameB) {
-					return -1;
-				}
-				if (nameA > nameB) {
-					return 1;
-				}
-				return 0;
-			}),
-		},
-		revalidate: 3600
-	};
+    const plugins = res.filter((res) => res.category === "Plugin");
+
+    return {
+        props: {
+            plugins: plugins.sort((a, b) => {
+                var nameA = a.title.toUpperCase();
+                var nameB = b.title.toUpperCase();
+                if (nameA < nameB) {
+                    return -1;
+                }
+                if (nameA > nameB) {
+                    return 1;
+                }
+                return 0;
+            }),
+        },
+        revalidate: 3600,
+    };
 }
